82bc1f6526cb957a4322564220122e8acdff4d93
Andre Martingo <andre.martingo@gmail.com>
2021-09-13 21:16:32 +0100

    Add min versions

--- Sources/pocketnetworking/NetworkLayer.swift
+++ Sources/pocketnetworking/NetworkLayer.swift
@@ -12,15 +12,11 @@ class NetworkLayer: NetworkLayerProtocol {
     }
     
     func request(with request: URLRequest) async -> Result<(HTTP.Response), NetworkingError> {
-        if #available(iOSApplicationExtension 15.0, *) {
-            do {
-                let (data, response) = try await session.data(for: request)
-                return .success(.init(urlRequest: request, data: data, httpURLResponse: response as? HTTPURLResponse))
-            } catch {
-                return .failure(.underlying(error, nil))
-            }
-        } else {
-            fatalError()
+        do {
+            let (data, response) = try await session.data(for: request)
+            return .success(.init(urlRequest: request, data: data, httpURLResponse: response as? HTTPURLResponse))
+        } catch {
+            return .failure(.underlying(error, nil))
         }
     }
 }
